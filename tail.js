ea67738bf3422452264e8d87e5c2fb6a39b7b690
Sarah Moss <sarah-moss@live.com>
2022-03-25 17:42:38 +0000

    Add test code

--- tail.js
+++ tail.js
@@ -20,25 +20,45 @@ const assertEqual = function(actual, expected) {
 // ---------------------------------------------------------------------------------------------------------
 
 let tail = function(array) {
+  let tailArray = []
   if (array.length > 1) {
-  array.shift();
-  return array;
-  } else if (array.length === 1) {
-    return array;
+  tailArray = array
+  tailArray.shift()
+  console.log(tailArray);
+  return tailArray;
   } else {
-    return array;
+    tailArray = array;
+    console.log(tailArray);
+    return tailArray;
   }
 };
 
 // ---------------------------------------------------------------------------------------------------------
 // test code
+// --- assertEqual does not work for comparing arrays
 // ---------------------------------------------------------------------------------------------------------
 
-assertEqual(tail([5,6,7]), [6,7]); // numbers
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]); // strings
-assertEqual(tail([]), []); // empty array
-assertEqual(tail(["Yo"]), "Yo"); // one string
-assertEqual(tail([1]), 1); // one number
+const words = ["Yo Yo", "Lighthouse", "Labs"]; // strings
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// ---------------------------------------------------------------------------------------------------------
+
+const nums = ([5,6,7]); // numbers
+tail(nums);
+assertEqual(nums.length, 3);
+
+// ---------------------------------------------------------------------------------------------------------
+
+const emptyArray = ([]);
+tail(emptyArray);
+assertEqual(emptyArray.length, 0);
+
+// ---------------------------------------------------------------------------------------------------------
+
+const oneArrayItem = ([1]);
+tail(oneArrayItem);
+assertEqual(oneArrayItem.length, 1);
 
 // ---------------------------------------------------------------------------------------------------------
 // developer notes
