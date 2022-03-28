b74682666999cf0ddb0832e72d47e6bf31c5a10c
Sarah Moss <sarah-moss@live.com>
2022-03-28 22:19:42 +0000

    Create without function + test code

--- without.js
+++ without.js
@@ -33,12 +33,13 @@ const assertArraysEqual = function(actual, expected) {
 
 const without = function(source, itemsToRemove) {
   for (let sourceIndex = 0; sourceIndex < source.length; sourceIndex++) {
-    for (itemsToRemoveIndex = 0; itemsToRemoveIndex < itemsToRemove.length; itemsToRemoveIndex++) {
-      
-
+    for (let itemsToRemoveIndex = 0; itemsToRemoveIndex < itemsToRemove.length; itemsToRemoveIndex++) {
+      if (source[sourceIndex] === itemsToRemove[itemsToRemoveIndex]){
+        source.splice(sourceIndex, 1);
+      }
     }
   }
-
+  return source;
 }
 
 // ---------------------------------------------------------------------------------------------------------
@@ -49,3 +50,12 @@ const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+console.log(without([4, 8, 9], [9])); // => [4, 8])
+console.log(without([4, 8, 9], [8])); // => [4, 9])
+console.log(without([4, 8, 9], [8, 9])); // => [4])
+
+// ---------------------------------------------------------------------------------------------------------
+// developer notes
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+// ---------------------------------------------------------------------------------------------------------
