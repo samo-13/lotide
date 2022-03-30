85746fabab8a848b7797eac458470982a38572e7
Sarah Moss <sarah-moss@live.com>
2022-03-30 04:17:03 +0000

    Attempt conditional statement with Array.isArray method

--- flatten.js
+++ flatten.js
@@ -1,50 +1,59 @@
-// ---------------------------------------------------------------------------------------------------------
-// lotide project
-// --- flatten function flattens a given array with other arrays inside, into a single-level array.
-// ---------------------------------------------------------------------------------------------------------
+// // ---------------------------------------------------------------------------------------------------------
+// // lotide project
+// // --- flatten function flattens a given array with other arrays inside, into a single-level array.
+// // ---------------------------------------------------------------------------------------------------------
 
-// ---------------------------------------------------------------------------------------------------------
-// eqArrays function
-// ---------------------------------------------------------------------------------------------------------
+// // ---------------------------------------------------------------------------------------------------------
+// // eqArrays function
+// // ---------------------------------------------------------------------------------------------------------
 
-const eqArrays = function(actual, expected) {
-  for (let index = 0; index < actual.length; index++) {
-    if (actual[index] !== expected[index]) {
-      return false;
-    }
-  } return true;
-};
+// const eqArrays = function(actual, expected) {
+//   for (let index = 0; index < actual.length; index++) {
+//     if (actual[index] !== expected[index]) {
+//       return false;
+//     }
+//   } return true;
+// };
 
-// ---------------------------------------------------------------------------------------------------------
-// assertArraysEqual function
-// ---------------------------------------------------------------------------------------------------------
+// // ---------------------------------------------------------------------------------------------------------
+// // assertArraysEqual function
+// // ---------------------------------------------------------------------------------------------------------
 
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`😃 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😡 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertArraysEqual = function(actual, expected) {
+//   if (eqArrays(actual, expected)) {
+//     console.log(`😃 Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`😡 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 // ---------------------------------------------------------------------------------------------------------
 // without function
 // ---------------------------------------------------------------------------------------------------------
 
 const flatten = function(input) {
-  let result = [].concat.apply([], input)
-  console.log(result);
-  return result;
+  let flattenedArray = [];
+
+  for (let i = 0; i < input.length; i++) {
+    if (Array.isArray(i)) {
+      flattenedArray.push(...flatten(i))
+    } else {
+      flattenedArray.push(i);
+    }
+  }
+  return flattenedArray;
 };
 
 // ---------------------------------------------------------------------------------------------------------
 // test code
 // ---------------------------------------------------------------------------------------------------------
 
-console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6])) // => [1, 2, 3, 4, 5, 6]
-console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6, 7]]), [1, 2, 3, 4, 5, 6, 7]))
-console.log(assertArraysEqual(flatten([1]), [1]))
-console.log(assertArraysEqual(flatten([]), []))
+console.log(flatten([1, 2, 3, 4, 5, [6]]));
+
+// console.log(assertArraysEqual(flatten([1, 2, 3, 4, 5, [6]]), [1, 2, 3, 4, 5, 6])) // => [1, 2, 3, 4, 5, 6]
+// console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5]), [1, 2, 3, 4, 5]))
+// console.log(assertArraysEqual(flatten([1]), [1]))
+// console.log(assertArraysEqual(flatten([]), []))
 
 // ---------------------------------------------------------------------------------------------------------
 // developer notes
