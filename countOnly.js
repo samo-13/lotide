78d8173001053d80e93ae6535653cc47a10f8408
Sarah Moss <sarah-moss@live.com>
2022-04-04 19:21:38 +0000

    Declare function

--- countOnly.js
+++ countOnly.js
@@ -1,6 +1,9 @@
 // ---------------------------------------------------------------------------------------------------------
 // lotide project
 // a function given an array and an object will return an object containing counts of everything that the input object listed.
+// --- only keys which have a truthy value should be counted in the resulting object. 
+// --- all other strings (either set to false or not included at all in the object) should not be counted. 
+// --- if a particular string is meant to be counted but does not exist in the input array (like "f" in the example above), it also does not have to be included in the final count.
 // ---------------------------------------------------------------------------------------------------------
 
 // ---------------------------------------------------------------------------------------------------------
@@ -18,3 +21,8 @@ const assertEqual = function(actual, expected){
 // ---------------------------------------------------------------------------------------------------------
 // countOnly function
 // ---------------------------------------------------------------------------------------------------------
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+}
