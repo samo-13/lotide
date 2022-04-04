5e158729537673777717b73584727143c9d0a94a
Sarah Moss <sarah-moss@live.com>
2022-04-04 23:02:16 +0000

    Implement countOnly function

--- countOnly.js
+++ countOnly.js
@@ -1,8 +1,8 @@
 // ---------------------------------------------------------------------------------------------------------
 // lotide project
 // a function given an array and an object will return an object containing counts of everything that the input object listed.
-// --- only keys which have a truthy value should be counted in the resulting object. 
-// --- all other strings (either set to false or not included at all in the object) should not be counted. 
+// --- only keys which have a truthy value should be counted in the resulting object.
+// --- all other strings (either set to false or not included at all in the object) should not be counted.
 // --- if a particular string is meant to be counted but does not exist in the input array (like "f" in the example above), it also does not have to be included in the final count.
 // ---------------------------------------------------------------------------------------------------------
 
@@ -10,7 +10,7 @@
 // assertEqual function
 // ---------------------------------------------------------------------------------------------------------
 
-const assertEqual = function(actual, expected){
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`😃 Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -20,18 +20,28 @@ const assertEqual = function(actual, expected){
 
 // ---------------------------------------------------------------------------------------------------------
 // countOnly function
-// allItems: an array of strings that we need to look through
-// itemsToCount: an object specifying what to count
+// --- allItems: an array of strings that we need to look through
+// --- itemsToCount: an object specifying what to count
 // ---------------------------------------------------------------------------------------------------------
 
 const countOnly = function(allItems, itemsToCount) {
-
-}
+  const results = {};
+  for (const item of allItems) {
+    console.log(item);
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
 
 // ---------------------------------------------------------------------------------------------------------
 // test code
 // ---------------------------------------------------------------------------------------------------------
-
 
 const firstNames = [
   "Karl",
@@ -51,4 +61,4 @@ assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
 assertEqual(result1["Agouhanna"], undefined);
-Question
+
