527e2525a67c18c98d8f86cac39eb120dd3631ce
Sarah Moss <sarah-moss@live.com>
2022-04-04 19:23:16 +0000

    Add test code

--- countOnly.js
+++ countOnly.js
@@ -20,9 +20,35 @@ const assertEqual = function(actual, expected){
 
 // ---------------------------------------------------------------------------------------------------------
 // countOnly function
-// ---------------------------------------------------------------------------------------------------------
-
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
+// ---------------------------------------------------------------------------------------------------------
+
 const countOnly = function(allItems, itemsToCount) {
+
 }
+
+// ---------------------------------------------------------------------------------------------------------
+// test code
+// ---------------------------------------------------------------------------------------------------------
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+Question
