33c1d07273e74b17cc1cfea57bee8f6bad53d19e
Sarah Moss <sarah-moss@live.com>
2022-04-06 18:15:37 +0000

    Add eqArrays

--- eqObjects.js
+++ eqObjects.js
@@ -6,6 +6,8 @@
 // --- Otherwise you get back a big fat false!
 // ---------------------------------------------------------------------------------------------------------
 
+const { is } = require("express/lib/request");
+
 // ---------------------------------------------------------------------------------------------------------
 // assertEqual function
 // ---------------------------------------------------------------------------------------------------------
@@ -42,8 +44,16 @@ const eqArrays = function(actual, expected) {
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
-  }
+  };
 
+  for (let key of Object.keys(object1)) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      // console.log(eqArrays(object1, object2))
+      return eqArrays(object1, object2);
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  } return true;
 };
 
 // ---------------------------------------------------------------------------------------------------------
@@ -71,6 +81,7 @@ assertEqual(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
+
 // ---------------------------------------------------------------------------------------------------------
 // developer notes
 // ---------------------------------------------------------------------------------------------------------
