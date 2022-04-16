0af9d5e317b86275b47eef835790b7502cafbeba
Sarah Moss <sarah-moss@live.com>
2022-04-16 19:09:02 +0000

    Lint tailTest.js code

--- test/tailTest.js
+++ test/tailTest.js
@@ -16,32 +16,32 @@ describe("#tail", () => {
   });
 
   it("returns [] for ['5']", () => {
-    assert.deepEqual(tail(['5']), []); 
+    assert.deepEqual(tail(['5']), []);
   });
 
   // numbers
   it("returns [6,7] for [5,6,7]", () => {
-    assert.deepEqual(tail([5,6,7]), [6,7]); 
+    assert.deepEqual(tail([5,6,7]), [6,7]);
   });
 
-  // strings 
+  // strings
   it("returns ['Lighthouse, Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
-    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]); 
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
   });
 
-  // empty array 
+  // empty array
   it("returns [] for []", () => {
-    assert.deepEqual(tail([]), []); 
+    assert.deepEqual(tail([]), []);
   });
 
   // one string
   it("returns [] for ['Yo']", () => {
-    assert.deepEqual(tail(["Yo"]), []); 
+    assert.deepEqual(tail(["Yo"]), []);
   });
 
   // one number
   it("returns [] for [1]", () => {
-    assert.deepEqual(tail([1]), []); 
+    assert.deepEqual(tail([1]), []);
   });
 
 });
