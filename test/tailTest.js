97cad232f2690f82102baf1cb10c2a7dc75f9dd8
Sarah Moss <sarah-moss@live.com>
2022-04-16 19:04:57 +0000

    Refactor tailTest using mocha & chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,26 +2,66 @@
 // require
 // ---------------------------------------------------------------------------------------------------------
 
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
 // ---------------------------------------------------------------------------------------------------------
 // test code
 // --- note assertEqual does not work for comparing arrays so test code does not check return value
 // ---------------------------------------------------------------------------------------------------------
 
-const words = ["Yo Yo", "Lighthouse", "Labs"]; // strings
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"]; // strings
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
 
-const nums = ([5,6,7]); // numbers
-tail(nums);
-assertEqual(nums.length, 3);
+// const nums = ([5,6,7]); // numbers
+// tail(nums);
+// assertEqual(nums.length, 3);
 
-const emptyArray = ([]);
-tail(emptyArray);
-assertEqual(emptyArray.length, 0);
+// const emptyArray = ([]);
+// tail(emptyArray);
+// assertEqual(emptyArray.length, 0);
 
-const oneArrayItem = ([1]);
-tail(oneArrayItem);
-assertEqual(oneArrayItem.length, 1);
+// const oneArrayItem = ([1]);
+// tail(oneArrayItem);
+// assertEqual(oneArrayItem.length, 1);
+
+// ---------------------------------------------------------------------------------------------------------
+// test code
+// ---------------------------------------------------------------------------------------------------------
+
+describe("#tail", () => {
+  it("returns 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
+
+  // numbers
+  it("returns [6,7] for [5,6,7]", () => {
+    assert.deepEqual(tail([5,6,7]), [6,7]); 
+  });
+
+  // strings 
+  it("returns ['Lighthouse, Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]); 
+  });
+
+  // empty array 
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []); 
+  });
+
+  // one string
+  it("returns [] for ['Yo']", () => {
+    assert.deepEqual(tail(["Yo"]), []); 
+  });
+
+  // one number
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []); 
+  });
+
+});
