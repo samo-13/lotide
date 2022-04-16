962836fecfc57f98a6f8ceff507e7fe78b09103b
Sarah Moss <sarah-moss@live.com>
2022-04-16 19:06:11 +0000

    Clean comments on tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -7,27 +7,7 @@ const tail = require('../tail');
 
 // ---------------------------------------------------------------------------------------------------------
 // test code
-// --- note assertEqual does not work for comparing arrays so test code does not check return value
-// ---------------------------------------------------------------------------------------------------------
-
-// const words = ["Yo Yo", "Lighthouse", "Labs"]; // strings
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// const nums = ([5,6,7]); // numbers
-// tail(nums);
-// assertEqual(nums.length, 3);
-
-// const emptyArray = ([]);
-// tail(emptyArray);
-// assertEqual(emptyArray.length, 0);
-
-// const oneArrayItem = ([1]);
-// tail(oneArrayItem);
-// assertEqual(oneArrayItem.length, 1);
-
-// ---------------------------------------------------------------------------------------------------------
-// test code
+// --- Note that for comparing arrays, Chai gives us assert.deepEqual, because assert.strictEqual simply uses === to compare values, which will not work for comparing objects and arrays.
 // ---------------------------------------------------------------------------------------------------------
 
 describe("#tail", () => {
