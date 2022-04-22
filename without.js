f7fa26286b765eb1654a1c93cae57e2603a37b42
Sarah Moss <sarah-moss@live.com>
2022-04-22 15:29:57 +0000

    Tweak without.js function to solve errors

--- without.js
+++ without.js
@@ -32,30 +32,28 @@ const assertArraysEqual = function(actual, expected) {
 // ---------------------------------------------------------------------------------------------------------
 
 const without = function(source, itemsToRemove) {
-  for (let sourceIndex = 0; sourceIndex < source.length; sourceIndex++) {
-    for (let itemsToRemoveIndex = 0; itemsToRemoveIndex < itemsToRemove.length; itemsToRemoveIndex++) {
-      if (source[sourceIndex] === itemsToRemove[itemsToRemoveIndex]){
-        source.splice(sourceIndex, 1);
-      }
-    }
-  }
-  return source;
-}
+  let filteredSource = source.filter((item) => !itemsToRemove.includes(item));
+  return filteredSource;
+};
 
 // ---------------------------------------------------------------------------------------------------------
 // test code
 // ---------------------------------------------------------------------------------------------------------
 
+// Ensuring that the original array was not altered by the without function
 const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+without(words, ["lighthouse"]);
+console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]));
 
+console.log(assertArraysEqual(without(words, ["lighthouse"]), ["hello", "world"]))
 console.log(without([4, 8, 9], [9])); // => [4, 8])
 console.log(without([4, 8, 9], [8])); // => [4, 9])
 console.log(without([4, 8, 9], [8, 9])); // => [4])
-
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without([1, 2, 3, 2, 1], [1, 2])); // => [3]
 // ---------------------------------------------------------------------------------------------------------
 // developer notes
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes// 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
 // ---------------------------------------------------------------------------------------------------------
